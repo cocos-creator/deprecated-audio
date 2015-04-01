5206bbff811032d43ec7a6bc55c4e51f10efa636
knoxHuang <821686174@qq.com>
2015-04-01 17:23:36 +0800

    update fireball-x/dev#506:audio api文档

--- src/audio-clip.js
+++ src/audio-clip.js
@@ -1,4 +1,10 @@
 ﻿Fire.AudioClip = (function () {
+
+    /**
+     * The audio clip is an audio source data.
+     * @class AudioClip
+     * @extends Asset
+     */
     var AudioClip = Fire.extend("Fire.AudioClip", Fire.Asset);
 
     AudioClip.prop('rawData', null, Fire.RawType('audio'), Fire.HideInInspector);
@@ -7,18 +13,42 @@
         return Fire.AudioContext.getClipBuffer(this);
     }, Fire.HideInInspector);
 
+    /**
+     * The length of the audio clip in seconds (Read Only).
+     * @property length
+     * @type {number}
+     * @readOnly
+     */
     AudioClip.get("length", function () {
         return Fire.AudioContext.getClipLength(this);
     });
 
+    /**
+     * The length of the audio clip in samples (Read Only).
+     * @property samples
+     * @type {number}
+     * @readOnly
+     */
     AudioClip.get("samples", function () {
         return Fire.AudioContext.getClipSamples(this);
     });
 
+    /**
+     * Channels in audio clip (Read Only).
+     * @property channels
+     * @type {number}
+     * @readOnly
+     */
     AudioClip.get("channels", function () {
         return Fire.AudioContext.getClipChannels(this);
     });
 
+    /**
+     * Sample frequency (Read Only).
+     * @property frequency
+     * @type {number}
+     * @readOnly
+     */
     AudioClip.get("frequency", function () {
         return Fire.AudioContext.getClipFrequency(this);
     });
